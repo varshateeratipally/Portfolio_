c5af974f3b8a1af5ada9c378f5d6167741506e6f
varshateeratipally <139103379+varshateeratipally@users.noreply.github.com>
2024-07-26 16:47:25 +0530

    Add files via upload

---
+++ server/core/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Contact
+
+
+@csrf_exempt
+def contact(request):
+    if request.method == 'POST':
+        print("vfdjv",request.POST)
+        first_name = request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        email=request.POST.get('email')
+        phone_number=request.POST.get('phone_number')
+        message=request.POST.get('message')
+        contact= Contact.objects.create(first_name = first_name,last_name=last_name,email=email,phone_number=phone_number,message=message)
+        print(contact)
+        return JsonResponse({'stauts':'ok'})
+    else:
+        print('this is a get request')
