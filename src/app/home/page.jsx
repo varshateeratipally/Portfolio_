089127c45ced530dddf2922d662d915fbaf992e2
varshateeratipally <varshateeratipally@gmail.com>
2024-07-29 20:07:49 +0530

    updated with skills

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,46 +3,94 @@
 import Image from "next/image";
 import styles from "./styles.module.css"
 import React from 'react';
-// import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-// import 'react-circular-progressbar/dist/styles.css';
-
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+
+const SkillsChart = () => {
+  const skills = [
+    { name: 'Painting', value: 57 },
+    { name: 'Edting', value: 20 },
+    { name: 'Business', value: 90 },
+    { name: 'Decorative', value: 50 },
+  ];
+
+  const horizontalSkills = [
+    { name: 'Creativity', value: 75 },
+    { name: 'Cooking', value: 30 },
+    { name: 'hced', value: 80 },
+    { name: 'jid', value: 20 },
+  ];
+
+  return (
+    <div className="skills-chart">
+      <div className="circular-skills">
+        {skills.map((skill, index) => (
+          <div key={index} className="skill">
+                  <div className="circle-container">
+                        <CircularProgressbar
+                          value={skill.value}
+                          text={`${skill.value}%`}
+                          styles={buildStyles({
+                            textColor: ' #482106',
+                            pathColor: '#D9A760',
+                            trailColor: '#D1D3D4',
+                            textSize: '16px',
+                            pathTransitionDuration: 0.5,
+                            strokeLinecap: 'butt',
+                            trailWidth: 4,
+                          })}
+                          strokeWidth={15} 
+                          />
+                          </div>
+            <p>{skill.name}</p>
+          </div>
+        ))}
+      </div>
+      <div className="horizontal-skills">
+        {horizontalSkills.map((skill, index) => (
+          <div key={index} className="horizontal-skill">
+            <div className="skill-name">
+              <p>{skill.name}</p>
+              <p>{skill.value}%</p>
+            </div>
+            <div className="skill-bar">
+              <div
+                className="skill-bar-fill"
+                style={{ width: `${skill.value}%` }}
+              ></div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+
 
 
 const Homepage = () => {
-  // const skills = [
-  //   { name: 'Painting', value: 57 },
-  //   { name: 'Edting', value: 20 },
-  //   { name: 'Business', value: 90 },
-  //   { name: 'Decorative', value: 50 },
-  // ];
 
-  // const horizontalSkills = [
-  //   { name: 'Creativity', value: 75 },
-  //   { name: 'Cooking', value: 30 },
-  //   { name: 'hced', value: 80 },
-  //   { name: 'jid', value: 20 },
-  // ];
   const [first_name, setfirst_name] = useState('')
   const [last_name, setlast_name] = useState('')
   const [email, setemail] = useState('')
   const [phone_number, setphone_number] = useState('')
   const [message, setmessage] = useState('');
-
-
-
   const fetchData = async () => {
-    const data = { first_name: first_name, last_name: last_name, email: email, phone_number: phone_number, message: message }
-    const formData= new FormData()
-    formData.set("first_name", first_name);
-    formData.set("last_name", last_name);
-    formData.set("email", email);
-    formData.set("phone_number", phone_number);
-    formData.set("message", message);
+   
+    const data = new URLSearchParams()
+    data.append("first_name", first_name);
+    data.append("last_name", last_name);
+    data.append("email", email);
+    data.append("phone_number", phone_number);
+    data.append("message", message);
     try {
       const result = await fetch('http://localhost:8000/contact', {
         method: "POST",
-        body: formData,
+        body: data,
         headers: { 'content-type': 'application/x-www-form-urlencoded' },
         mode: 'no-cors'
       })
@@ -58,12 +106,6 @@ const Homepage = () => {
   const handleSubmit = () => {
     fetchData();
   }
-
-
-
-
-
-
 
   return (
     <div className={styles.container}>
@@ -139,48 +181,10 @@ const Homepage = () => {
           </div>
         </div>
         {/* skills */}
-        {/* <div className="skills-chart">
-      <div className="circular-skills">
-        {skills.map((skill, index) => (
-          <div key={index} className="skill">
-                  <div className="circle-container">
-                        <CircularProgressbar
-                          value={skill.value}
-                          text={`${skill.value}%`}
-                          styles={buildStyles({
-                            textColor: ' #482106',
-                            pathColor: '#D9A760',
-                            trailColor: '#D1D3D4',
-                            textSize: '16px',
-                            pathTransitionDuration: 0.5,
-                            strokeLinecap: 'butt',
-                            trailWidth: 4,
-                          })}
-                          strokeWidth={15} 
-                          />
-                          </div>
-            <p>{skill.name}</p>
-          </div>
-        ))}
-      </div>
-      <div className="horizontal-skills">
-        {horizontalSkills.map((skill, index) => (
-          <div key={index} className="horizontal-skill">
-            <div className="skill-name">
-              <p>{skill.name}</p>
-              <p>{skill.value}%</p>
-            </div>
-            <div className="skill-bar">
-              <div
-                className="skill-bar-fill"
-                style={{ width: `${skill.value}%` }}
-              ></div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
- */}
+        <SkillsChart />
+
+       
+
 
         {/* contact */}
         <div className={styles.container3}>
@@ -200,6 +204,6 @@ const Homepage = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 export default Homepage;
